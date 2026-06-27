c74597d626b63f0a73a4eb4295a3d8afcebbd490
platowashere666-ship-it <platowashere666@gmail.com>
2026-06-27 16:15:51 +0300

    Задача CSS Arrays правил по списку

--- JsArrays/Arrays.js
+++ JsArrays/Arrays.js
@@ -1,39 +1,41 @@
 "use strict";
 
 (function () {
-    const array = [4, 2, 1, 7, 6, 8, 4, 2, 1, 14, 643, 1000, 3];
+    const numbers = [4, 2, 1, 7, 6, 8, 4, 2, 1, 14, 643, 1000, 3];
 
-    array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
+    numbers.sort((e1, e2) => e2 - e1);
 
-    console.log("Отсортированный по убыванию массив: " + array);
+    console.log("Отсортированный по убыванию массив: " + numbers);
 
-    const subArray = array.slice(0, 5);
-    console.log("Подмассив из первых 5 элементов: " + subArray);
+    const largestFiveNumbers = numbers.slice(0, 5);
+    console.log("Подмассив из первых 5 элементов: " + largestFiveNumbers);
 
-    const subArray2 = array.reverse().slice(0, 5);
-    console.log("Подмассив из последних 5 элементов: " + subArray2);
+    const smallestFiveNumbers = numbers.slice(-5);
+    console.log("Подмассив из последних 5 элементов: " + smallestFiveNumbers);
 
-    function getEvenNumbersSum(array) {
-        const evenNumbersArray = array.filter(e => e % 2 === 0);
+    function getEvenNumbersSum(numbers) {
+        return numbers.reduce((sum, e) => {
+            if (e % 2 === 0) {
+                return sum + e;
+            }
 
-        return evenNumbersArray.reduce((sum, current) => sum + current, 0);
+            return sum;
+        }, 0);
     }
 
-    console.log("Сумма чётных элементов массива: " + getEvenNumbersSum(array));
+    console.log("Сумма чётных элементов массива: " + getEvenNumbersSum(numbers));
 
-    const array2 = [];
+    const numbersFromOneToHundred = [];
 
     for (let i = 1; i <= 100; ++i) {
-        array2.push(i);
+        numbersFromOneToHundred.push(i);
     }
 
-    function getEvenNumbersSquaresArray(array) {
-        return array
+    function getEvenNumbersSquaresArray(numbers) {
+        return numbers
             .filter(e => e % 2 === 0)
             .map(e => e * e);
     }
 
-    console.log("Список квадратов чётных элементов второго массива: " + getEvenNumbersSquaresArray(array2));
+    console.log("Список квадратов чётных элементов второго массива: " + getEvenNumbersSquaresArray(numbersFromOneToHundred));
 })();
