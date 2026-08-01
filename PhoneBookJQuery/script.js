355ade8399caedd856625dccb75e6374025d53b3
platowashere666-ship-it <platowashere666@gmail.com>
2026-08-01 19:38:49 +0300

    Задача PhoneBook JQuery правил по списку

--- PhoneBookJQuery/script.js
+++ PhoneBookJQuery/script.js
@@ -7,8 +7,6 @@ $(function () {
     const newContactName = $("#new-contact-name");
     const newContactPhoneNumber = $("#new-contact-phone-number");
 
-    let currentContactId = 1;
-
     function validateFieldValue(field) {
         field.removeClass("is-invalid");
 
@@ -17,17 +15,23 @@ $(function () {
         if (fieldValue.length === 0) {
             field.addClass("is-invalid");
             return null;
-        } else {
-            return fieldValue;
         }
+
+        return fieldValue;
+    }
+
+    function updateRowsNumbers() {
+        tableBody.find("tr").each(function (index) {
+            $(this).find(".row-number").text(index + 1);
+        });
     }
 
     form.on("submit", function (e) {
         e.preventDefault();
 
-        let newContactSurnameValue = validateFieldValue(newContactSurname);
-        let newContactNameValue = validateFieldValue(newContactName);
-        let newContactPhoneNumberValue = validateFieldValue(newContactPhoneNumber);
+        const newContactSurnameValue = validateFieldValue(newContactSurname);
+        const newContactNameValue = validateFieldValue(newContactName);
+        const newContactPhoneNumberValue = validateFieldValue(newContactPhoneNumber);
 
         if (newContactSurnameValue === null || newContactNameValue === null || newContactPhoneNumberValue === null) {
             return;
@@ -35,67 +39,64 @@ $(function () {
 
         const newContactRow = $("<tr>");
 
-        function setViewMode() {
-            const newContactId = newContactRow.data('new-contact-id') || currentContactId;
-
+        function setViewMode(surname, name, phoneNumber) {
             newContactRow.html(`
-                <th scope="row" class="new-contact-id">${newContactId}</th>
+                <th scope="row" class="row-number"></th>
                 <td class="new-contact-surname"></td>
                 <td class="new-contact-name"></td>
                 <td class="new-contact-phone-number"></td>
                 <td>
-                    <button class="btn btn-sm btn-warning edit-button" type="button">
+                    <button class="btn btn-sm btn-warning edit-button" title="Редактировать" type="button">
                         <i class="bi bi-pencil"></i>
                     </button>
-                    <button class="btn btn-sm btn-danger delete-button" type="button">
+                    <button class="btn btn-sm btn-danger delete-button" title="Удалить" type="button">
                         <i class="bi bi-trash"></i>
                     </button>
                 </td>
             `);
 
-            newContactRow.data('new-contact-id', newContactId);
-
-            newContactRow.find(".new-contact-surname").text(newContactSurnameValue);
-            newContactRow.find(".new-contact-name").text(newContactNameValue);
-            newContactRow.find(".new-contact-phone-number").text(newContactPhoneNumberValue);
+            newContactRow.find(".new-contact-surname").text(surname);
+            newContactRow.find(".new-contact-name").text(name);
+            newContactRow.find(".new-contact-phone-number").text(phoneNumber);
 
             newContactRow.find(".edit-button").on("click", function () {
-                const editContactId = newContactRow.data("new-contact-id");
+                const currentRowNumber = newContactRow.find(".row-number").text();
 
                 newContactRow.html(`
-                    <th scope="row" class="edit-contact-id">${editContactId}</th>
+                    <th scope="row" class="row-number">${currentRowNumber}</th>
             
                     <td>
-                        <input class="form-control edit-contact-surname" type="text" placeholder="Иван">
+                        <input class="form-control edit-contact-surname" type="text" placeholder="Фамилия">
                         <div class="invalid-feedback">Необходимо заполнить поле "Фамилия"</div>
                     </td>
             
                     <td>
-                        <input class="form-control edit-contact-name" type="text" placeholder="Иванов">
+                        <input class="form-control edit-contact-name" type="text" placeholder="Имя">
                         <div class="invalid-feedback">Необходимо заполнить поле "Имя"</div>
                     </td>
             
                     <td>
-                        <input class="form-control edit-contact-phone-number" type="tel" placeholder="+7__________">
+                        <input class="form-control edit-contact-phone-number" type="tel" placeholder="Номер телефона">
                         <div class="invalid-feedback">Необходимо заполнить поле "Номер телефона"</div>
                     </td>
             
                     <td>
-                        <button class="btn btn-sm btn-success save-button" type="button">
+                        <button class="btn btn-sm btn-success save-button" title="Сохранить" type="button">
                             <i class="bi bi-check-lg"></i>
                         </button>
-                        <button class="btn btn-sm btn-secondary cancel-button" type="button">
+                        <button class="btn btn-sm btn-secondary cancel-button" title="Отменить" type="button">
                             <i class="bi bi-x-lg"></i>
                         </button>
                     </td>
                 `);
 
-                newContactRow.find(".edit-contact-surname").val(newContactSurnameValue);
-                newContactRow.find(".edit-contact-name").val(newContactNameValue);
-                newContactRow.find(".edit-contact-phone-number").val(newContactPhoneNumberValue);
+                newContactRow.find(".edit-contact-surname").val(surname);
+                newContactRow.find(".edit-contact-name").val(name);
+                newContactRow.find(".edit-contact-phone-number").val(phoneNumber);
 
                 newContactRow.find(".cancel-button").on("click", function () {
-                    setViewMode();
+                    setViewMode(surname, name, phoneNumber);
+                    updateRowsNumbers();
                 });
 
                 newContactRow.find(".save-button").on("click", function () {
@@ -111,23 +112,21 @@ $(function () {
                         return;
                     }
 
-                    newContactSurnameValue = editContactSurnameValue;
-                    newContactNameValue = editContactNameValue;
-                    newContactPhoneNumberValue = editContactPhoneNumberValue;
-
-                    setViewMode();
+                    setViewMode(editContactSurnameValue, editContactNameValue, editContactPhoneNumberValue);
+                    updateRowsNumbers();
                 });
             });
 
             newContactRow.find(".delete-button").on("click", function () {
                 newContactRow.remove();
+                updateRowsNumbers();
             });
         }
 
-        setViewMode();
+        setViewMode(newContactSurnameValue, newContactNameValue, newContactPhoneNumberValue);
         tableBody.append(newContactRow);
+        updateRowsNumbers();
 
         form[0].reset();
-        ++currentContactId;
     });
 });
